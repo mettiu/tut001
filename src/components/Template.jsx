0dfa0df527dfc9c16c8a4d0b446a7c5101dd9bf7
Mettiu <mettiu@gmail.com>
2017-12-16 19:10:46 +0100

    completed tut 41 redux setup and working

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './shared/Header';
-import HomePage from './home/HomePage';
+import HomePageCont from './home/HomePageContainer';
 import ProfilePage from './account/ProfilePage';
 import LoginPage from './account/LoginPage';
 
-export default function Template() {
+export default function Template(props) {
   return (
     <Router>
       <div className="wrapper">
+        <p>{props.progress}</p>
         <Header username="anonymous" />
         <section className="page-content container-fluid">
-          <Route exact path="/" component={HomePage} />
+          <Route exact path="/" component={HomePageCont} />
           <Route path="/account/profile/:id" component={ProfilePage} />
           <Route exact path="/account/login" component={LoginPage} />
         </section>
