e36d1515847b2d44f4a5eefe1a1c18926c2c41b9
Mettiu <mettiu@gmail.com>
2017-12-16 20:25:13 +0100

    completed tut 44 added redux tools & prod optimization

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,34 @@
 // Default export from a module
 import React from 'react';
-// Individual method exports from a module
+
+// Individual exports from a module
 import { AppContainer } from 'react-hot-loader';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 
 // CSS from a module
 import 'bootstrap/dist/css/bootstrap.css';
+
 // CSS from a local file
 import './css/musiclist.scss';
 
 // Default export from a local file
-import Store from './store';
+import DevTools from './components/shared/DevTools';
+import configureStore from './store';
 
+// JSX Modules
 import TemplateContainer from './components/TemplateContainer';
+
+const Store = configureStore();
 
 const renderApp = (Component) => {
   render(
     <AppContainer>
       <Provider store={Store}>
-        <Component />
+        <div>
+          <Component />
+          <DevTools />
+        </div>
       </Provider>
     </AppContainer>,
     document.querySelector('#react-app'),
