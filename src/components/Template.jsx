f099b326a363f79256537565e382c976cf36cd36
Mettiu <mettiu@gmail.com>
2018-01-02 18:32:09 +0100

    tut 57

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HeaderContainer from './shared/HeaderContainer';
+import ErrorBox from './shared/ErrorBoxContainer';
 import HomePageCont from './home/HomePageContainer';
 import ProfilePage from './account/ProfilePage';
 import LoginPage from './account/LoginPageContainer';
@@ -14,6 +15,7 @@ export default function Template(props) {
       <div className="wrapper">
         <HeaderContainer username="anonymous" authentication={authentication} />
         <section className="page-content container-fluid">
+          <ErrorBox />
           <Route exact path="/" component={HomePageCont} />
           <Route path="/account/profile/:id" component={ProfilePage} />
           <Route exact path="/account/login" component={LoginPage} />
